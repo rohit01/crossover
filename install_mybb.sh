2a8d469e07b66609c2ba68e56135cde0e5bc2bc5
rohit01 <rohit.kgec@gmail.com>
2015-11-24 20:49:11 +0530

    Added DB initialization using basic dump

--- install_mybb.sh
+++ install_mybb.sh
@@ -18,15 +18,15 @@ sed -i "s/REPLACE_WITH_DB_HOSTNAME/${DB_HOSTNAME}/g" ./inc/config.php
 sed -i "s/REPLACE_WITH_DB_USER/${DB_USER}/g" ./inc/config.php
 sed -i "s/REPLACE_WITH_DB_PASSWORD/${DB_PASSWORD}/g" ./inc/config.php
 
+chmod 666 ./inc/settings.php
+chmod 666 ./inc/config.php
+chmod 666 ./inc/languages/english/*
+chmod 666 ./inc/languages/english/admin/*
+chmod 777 ./cache/
+chmod 777 ./cache/themes/
+chmod 777 ./uploads/
+chmod 777 ./uploads/avatars/
+chmod 777 ./admin/backups/ 
 
-# chmod 666 ./inc/settings.php
-# mv ./inc/config.default.php ./inc/config.php 
-# chmod 666 ./inc/config.php
-# chmod 666 ./inc/languages/english/*
-# chmod 666 ./inc/languages/english/admin/*
-# chmod 777 ./cache/
-# chmod 777 ./cache/themes/
-# chmod 777 ./uploads/
-# chmod 777 ./uploads/avatars/
-# chmod 777 ./admin/backups/ 
-
+cd "${SCRIPT_DIR}/"
+mysql -u "${DB_USER}" -h "${DB_HOSTNAME}" -p"${DB_PASSWORD}" "${DATABASE}" < mybb_initdb.sql 2>/dev/null || echo "DB already initialized. Ignoring import errors"
